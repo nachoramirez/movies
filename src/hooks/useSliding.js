11642f45794ecb85a85a6e0e590f4c534587af56
Nacho Ramirez <nachor7803@gmail.com>
2021-07-12 17:52:23 -0300

    fixing the handleprev in the movie carrousel

--- src/hooks/useSliding.js
+++ src/hooks/useSliding.js
@@ -11,10 +11,11 @@ const useSlibing = (children, clones) => {
 
   const totalInViewport = Math.floor(getWindowWidth() / ItemWidth)
   const totalItems = React.Children.count(children)
+  const totalSlides = Math.floor(totalItems / totalInViewport)
 
   //creates an array with total transitions made by the slider
   const dotsToShow = []
-  for (let i = 0; i < totalItems / totalInViewport; i++) {
+  for (let i = 0; i <= totalSlides; i++) {
     dotsToShow.push(i)
   }
 
@@ -24,11 +25,10 @@ const useSlibing = (children, clones) => {
       setTranslate(translate + ItemWidth * totalInViewport)
       setViewed(viewed + totalInViewport)
       setActiveDot(activeDot + 1)
-      console.log('next:', translate, viewed, initialTranslate)
     } else {
       //if the total viewed is higher than total items,
       //the slider slides to the element after the last slide
-      setTranslate(translate + ItemWidth * (totalItems - viewed))
+      setTranslate(translate + ItemWidth * (totalItems - totalSlides * totalInViewport))
       setViewed(viewed + totalInViewport)
       setTransition(300)
       setActiveDot(0)
@@ -42,24 +42,26 @@ const useSlibing = (children, clones) => {
   }
 
   const handlePrev = () => {
-    if (viewed  > 0) {
+    if (viewed > 0) {
       setTransition(300)
       setTranslate(translate - ItemWidth * totalInViewport)
       setViewed(viewed - totalInViewport)
       setActiveDot(activeDot - 1)
-    } else{
-      //if the viewed items is lower than 0 
+    } else {
+      //if the viewed items is lower than 0
       //the slider slides to the previus slide (clone)
       setTransition(300)
-      setTranslate(translate - ItemWidth * totalInViewport)
+      setTranslate(
+        translate - ItemWidth * (totalItems - totalSlides * totalInViewport)
+      )
       setActiveDot(dotsToShow.length - 1)
       //and then slides to the real childen without transition
       setTimeout(() => {
         setTransition(0)
         setTranslate(
-          (clones - 1 + totalItems - viewed - totalInViewport) * ItemWidth
+          initialTranslate + totalInViewport * ItemWidth * totalSlides
         )
-        setViewed(totalItems - viewed - 4)
+        setViewed((totalItems / totalInViewport - 1) * totalInViewport)
       }, 300)
     }
   }
